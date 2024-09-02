e578c553f0c41643a99e99fc8a1d45fd40281025
Arpit Malik <arpit.malik@paytmmoney.com>
2024-09-02 22:43:38 +0530

    feat(main): added beta and prod build scripts

--- packages/library/src/utils/__tests__/logsUtils.test.js
+++ packages/library/src/utils/__tests__/logsUtils.test.js
@@ -11,7 +11,7 @@ import {
 } from '../logsUtils';
 
 describe('logUtils unit tests', () => {
-  const originalNodeEnv = process.env.NODE_ENV;
+  const originalNodeEnv = process.env.APP_ENV;
 
   beforeEach(() => {
     jest.resetModules();
@@ -19,55 +19,55 @@ describe('logUtils unit tests', () => {
   });
 
   afterEach(() => {
-    process.env.NODE_ENV = originalNodeEnv;
+    process.env.APP_ENV = originalNodeEnv;
   });
 
   it('log unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     log('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     log('test');
   });
 
   it('errorLog unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     errorLog('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     errorLog('test');
   });
 
   it('warnLog unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     warnLog('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     warnLog('test');
   });
 
   it('debugLog unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     debugLog('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     debugLog('test');
   });
 
   it('traceLog unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     traceLog('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     traceLog('test');
   });
 
   it('tableLog unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     tableLog('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     tableLog('test');
   });
 
   it('infoLog unit test', () => {
-    process.env.NODE_ENV = 'development';
+    process.env.APP_ENV = 'development';
     infoLog('test');
-    process.env.NODE_ENV = 'production';
+    process.env.APP_ENV = 'production';
     infoLog('test');
   });
 });
--- packages/library/src/utils/logsUtils.js
+++ packages/library/src/utils/logsUtils.js
@@ -1,49 +1,49 @@
 import { ENVS } from '../enums/app';
 
 const log = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.log(...args);
   }
 };
 
 const errorLog = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.error(...args);
   }
 };
 
 const warnLog = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.warn(...args);
   }
 };
 
 const debugLog = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.debug(...args);
   }
 };
 
 const traceLog = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.trace(...args);
   }
 };
 
 const tableLog = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.table(...args);
   }
 };
 
 const infoLog = (...args) => {
-  if (process.env.NODE_ENV !== ENVS.PROD) {
+  if (process.env.APP_ENV !== ENVS.PROD) {
     // eslint-disable-next-line no-console
     console.info(...args);
   }

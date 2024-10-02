afa7299bdd23950154e35b5844e03332a2549e07
Arpit Malik <arpit08malik2000@gmail.com>
2024-10-02 23:07:08 +0530

    fix(services):  fixed sw registration utility

--- packages/library/src/utils/__tests__/logsUtils.test.js
+++ packages/library/src/utils/__tests__/logsUtils.test.js
@@ -13,6 +13,7 @@ import {
   tableLog,
   infoLog,
 } from '../logsUtils';
+import { ENVS } from '../../enums/app';
 
 describe('logUtils unit tests', () => {
   const originalNodeEnv = process.env.APP_ENV;
@@ -27,51 +28,51 @@ describe('logUtils unit tests', () => {
   });
 
   it('log unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     log('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     log('test');
   });
 
   it('errorLog unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     errorLog('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     errorLog('test');
   });
 
   it('warnLog unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     warnLog('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     warnLog('test');
   });
 
   it('debugLog unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     debugLog('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     debugLog('test');
   });
 
   it('traceLog unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     traceLog('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     traceLog('test');
   });
 
   it('tableLog unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     tableLog('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     tableLog('test');
   });
 
   it('infoLog unit test', () => {
-    process.env.APP_ENV = 'development';
+    process.env.APP_ENV = ENVS.DEV;
     infoLog('test');
-    process.env.APP_ENV = 'production';
+    process.env.APP_ENV = ENVS.PROD;
     infoLog('test');
   });
 });

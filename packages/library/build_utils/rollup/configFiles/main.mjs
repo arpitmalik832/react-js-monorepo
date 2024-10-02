fa862bb4303d47a78985be5f455781df06633e17
Arpit Malik <arpit08malik2000@gmail.com>
2024-10-03 01:08:17 +0530

    fix(main): allowing logs even in prod env of the library

--- packages/library/build_utils/rollup/configFiles/main.mjs
+++ packages/library/build_utils/rollup/configFiles/main.mjs
@@ -85,7 +85,7 @@ if (['production', 'beta'].includes(process.env.LIB_ENV)) {
       compress: {
         dead_code: true,
         drop_debugger: true,
-        drop_console: process.env.LIB_ENV === 'production',
+        drop_console: false,
         conditionals: true,
         evaluate: true,
         booleans: true,
